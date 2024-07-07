a44895f818c434a4028495f1b13f223ea86b49bc
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-07-07 10:29:54 +0530

    Tested deployment, works fine

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.26",
+  networks: {
+    ganache: {
+      url: process.env.PROVIDER_URL,
+      account: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
